f06919a0181212a610c5050a803cd0a359ef8a55
janhyeonseo <liom0987@gmail.com>
2024-09-10 14:34:29 +0900

    결제 완료, 로그인 기능 추가중

--- src/main/java/com/majustory/controller/LoginController.java
+++ src/main/java/com/majustory/controller/LoginController.java
@@ -3,13 +3,17 @@ package com.majustory.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/login")
 @Controller
 public class LoginController {
-	
 	
 	LoginController(){
 		System.out.println("==> LoginController 생성자");
 	}
 	
+	@GetMapping("/loginform")
+	private void member(){
+	}
 }
